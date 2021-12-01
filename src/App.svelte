8e5c816821aa5a1fa6639c7dbb2cccb61f4844b5
Warren Snipes <contact@warrensnipes.dev>
2021-12-01 01:26:32 -0500

    Changed font overhang on mobile

--- src/App.svelte
+++ src/App.svelte
@@ -178,8 +178,8 @@
     main {
         text-align: center;
         padding: 1em;
-        max-width: 240px;
         margin: 0 auto;
+        max-width: none;
     }
 
     select {
@@ -189,13 +189,7 @@
     h1 {
         color: #ff3e00;
         text-transform: uppercase;
-        font-size: 4em;
+        font-size: 6vw;
         font-weight: 100;
-    }
-
-    @media (min-width: 640px) {
-        main {
-            max-width: none;
-        }
     }
 </style>
