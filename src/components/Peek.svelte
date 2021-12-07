f0513e23480aeadb87ccc17901b78eff7ad4df89
Aaron Nguyen <me@aaronnguyen.dev>
2021-12-07 12:39:03 -0500

    Remove placeholder title + add dynamic URL iframe

--- src/components/Peek.svelte
+++ src/components/Peek.svelte
@@ -1,13 +1,22 @@
 <script lang="ts">
+  import { selectedRow } from "../stores/selectedRow";
 </script>
 
 <div>
-  <h2 class="title">Peek</h2>
-  <iframe
-    id="iframe"
-    src="https://www.ratemyprofessors.com/ShowRatings.jsp?tid=232236"
-    title="Rate My Professors"
-  />
+  <h2 class="title">Reviews</h2>
+  {#if $selectedRow === ""}
+    <h3
+      class="is-size-5 has-text-grey has-text-centered has-text-weight-medium"
+    >
+      Just chilling...
+    </h3>
+  {:else}
+    <iframe
+      id="iframe"
+      src={`https://www.ratemyprofessors.com/ShowRatings.jsp?tid=${$selectedRow}`}
+      title="Rate My Professors"
+    />
+  {/if}
 </div>
 
 <style>
